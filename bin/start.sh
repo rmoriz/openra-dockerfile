dc2dada485789cbe6ad4da61d0c439ed0596ceb2
Roland Moriz <roland@moriz.de>
2014-12-27 13:26:47 +0100

    make password + map dynamic

--- bin/start.sh
+++ bin/start.sh
@@ -3,14 +3,16 @@
 # see https://github.com/OpenRA/OpenRA/wiki/Dedicated
 #
 Name="${NAME:-"Dedicated-Server"}"
-Mod=${MOD:-"ra"} #
+Mod=${MOD:-"ra"}
 LockBots=${LOCK_BOTS:-"False"}
 Ban=${BAN:-""}
 Dedicated="True"
 DedicatedLoop="True" # A new instance is spawned once previous game is finished
 ListenPort=1234
-ExternalPort=${EXTERNAL_PORT:-"1234"}
+ExternalPort="${EXTERNAL_PORT:-"1234"}"
 AdvertiseOnline=${ADVERTISE_ONLINE:-"False"}
+Map="${MAP}"
+Password="${PASSWORD}"
 
 MOTD="${MOTD:-"welcome to a Docker based OpenRA server"}"
 echo $MOTD > /home/openra/.openra/motd.txt
@@ -18,8 +20,21 @@ echo $MOTD > /home/openra/.openra/motd.txt
 # if no maps volume is mounted, sync the latest maps from the community rsync service
 [ "$(ls -A /home/openra/.openra/maps/)" ] && echo "will use existing maps" || rsync --delete -avp rsync://resource.openra.net/maps/ /home/openra/.openra/maps/
 
+echo "=================================================================="
+echo "MOD:              $Mod"
+echo "BAN:              $Ban"
+echo "MAP:              $Map"
+echo "NAME:             $Name"
+echo "PASSWORD:         $Password"
+echo "LOCK_BOTS:        $LockBots"
+echo "EXTERNAL_PORT:    $ExternalPort"
+echo "ADVERTISE_ONLINE: $AdvertiseOnline"
+echo "=================================================================="
+
 mono --debug /usr/lib/openra/OpenRA.Game.exe Game.Mod=$Mod Server.Dedicated=$Dedicated Server.DedicatedLoop=$DedicatedLoop \
 Server.Name="$Name" Server.ListenPort=$ListenPort Server.ExternalPort=$ExternalPort \
 Server.LockBots=$LockBots \
 Server.Ban="$Ban" \
-Server.AdvertiseOnline=$AdvertiseOnline
+Server.AdvertiseOnline=$AdvertiseOnline \
+Server.Map=$Map \
+Server.Password=$Password
