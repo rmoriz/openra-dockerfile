ca146c05fcd0128e71534b488a478bf049c1a871
Roland Moriz <roland@moriz.de>
2020-02-01 21:15:07 +0100

    attempt to fix qemu

--- hooks/get-qemu.sh
+++ hooks/get-qemu.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+set -ex
 
-PACKAGE=http://ftp.de.debian.org/debian/pool/main/q/qemu/qemu-user-static_4.1-1+b4_amd64.deb
+# NOTE: this url will change regularly because it's unstable
+PACKAGE=http://ftp.de.debian.org/debian/pool/main/q/qemu/qemu-user-static_4.2-2_amd64.deb
 
 mkdir tmp/
 cd tmp/
+
 curl $PACKAGE -o $(basename ${PACKAGE})
 dpkg-deb -X $(basename ${PACKAGE}) .
 cp usr/bin/qemu-aarch64-static ..
